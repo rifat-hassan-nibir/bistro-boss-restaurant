eed41f150cde163b8aef05147f6054e693e87cda
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-17 11:11:36 +0600

    added basic navbar and footer

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Pages/Shared/Navbar";
+import Footer from "../Pages/Shared/Footer";
 
 const MainLayout = () => {
   return (
     <div>
-      {/* Navbar */}
-
-      {/* Body */}
-      <Outlet></Outlet>
-
-      {/* Footer */}
+      <Navbar></Navbar>
+      <div className="min-h-[60vh]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
