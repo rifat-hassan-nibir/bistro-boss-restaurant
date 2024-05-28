ae3a0c1087c98cc7390bc5bf2d73d7aa25f73251
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-05-28 10:28:33 +0600

    added table to dashboard

--- src/Pages/Dashboard/Cart.jsx
+++ src/Pages/Dashboard/Cart.jsx
@@ -1,7 +1,44 @@
+import useCart from "../../Hooks/useCart";
+
 const Cart = () => {
+  const [cart] = useCart();
+
   return (
     <div>
-      <h1>Cart</h1>
+      <div className="flex justify-between py-10 px-[100px] text-2xl font-semibold">
+        <h1>Total Items: {cart.length}</h1>
+        <h1>Total Price: {cart.reduce((total, item) => total + item.price, 0)}</h1>
+        <button className="btn">Pay</button>
+      </div>
+      {/* Table */}
+      <div className="overflow-x-auto px-10">
+        <table className="table table-zebra">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <img src={item.image} className="size-16"></img>
+                </td>
+                <td>Quality Control Specialist</td>
+                <td>Blue</td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
